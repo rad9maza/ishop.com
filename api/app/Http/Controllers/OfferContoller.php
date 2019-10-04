c9d3f1f2718c1d5429a55ed87eb8b4dfa850c7bc
Rodion Bolkunevych <rbolck@gmail.com>
2019-10-04 15:10:53 +0300

    Add controllers

---
+++ api/app/Http/Controllers/OfferContoller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OfferContoller extends Controller
+{
+    //
+}
---
+++ api/app/Http/Controllers/ProductContoller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductContoller extends Controller
+{
+    public function index()
+    {
+        dd('1');
+    }
+
+    public function create()
+    {
+        dd('1');
+    }
+
+    public function store()
+    {
+        dd('1');
+    }
+
+    public function show($id)
+    {
+        dd($id);
+    }
+
+    public function edit($id)
+    {
+        dd($id);
+    }
+
+    public function update($id)
+    {
+        dd($id);
+    }
+
+    public function destroy($id)
+    {
+        dd($id);
+    }
+}
