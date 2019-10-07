e50b189632e4ee26efd328654782bc57093e267e
Rodion Bolkunevych <rbolck@gmail.com>
2019-10-07 18:07:10 +0300

    feat(mariadb): add YbrNX\MariaDB

--- api/config/app.php
+++ api/config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        YbrNX\MariaDB\MariaDBServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
